a8a2df2c405058907432aa9720804eb045271669
Thad Cole <thadjcole@gmail.com>
2020-05-30 17:14:16 -0400

    input validation added to character type choices -> at least 1 has to be chosen

--- Develop/script.js
+++ Develop/script.js
@@ -16,10 +16,10 @@ function writePassword() {
 // returns password(str)
 function generatePassword() {
   // length of password between 8 and 128
-  const passwordLengthPrompt = 
+  const passwordLengthPrompt =
     "Choose your desired password length (8-128 characters).\n" +
     "Default length is 8 characters."
-  const passwordLength = prompt(passwordLengthPrompt)
+  let passwordLength = prompt(passwordLengthPrompt)
   // validate passwordLength user input
   if (passwordLength > 128) {
     passwordLength = 128;
@@ -30,44 +30,74 @@ function generatePassword() {
   console.log("passwordLength", passwordLength);
 
   // series of confirms to pick various character types
-  const lowercasePrompt =
-    "Does your password require lowercase characters?";
-  const lowercase = confirm(lowercasePrompt);
-  console.log("lowercase", lowercase);
+  // initialize choices to be used outside do-while loop
+  let lowercase = true;
+  let uppercase = false;
+  let numeric = false;
+  let special = false; 
+  do {
+    const charTypePrompt =
+      "Please select at least 1 of the 4 following character types";
+    alert(charTypePrompt);
+
+    const lowercasePrompt =
+      "Does your password require lowercase characters?";
+    lowercase = confirm(lowercasePrompt);
+    console.log("lowercase", lowercase);
+
+    const uppercasePrompt =
+      "Does your password require uppercase characters?";
+    uppercase = confirm(uppercasePrompt);
+    console.log("uppercase", uppercase);
+
+    const numericPrompt =
+      "Does your password require numeric characters?";
+    numeric = confirm(numericPrompt);
+    console.log("numeric", numeric);
+
+    const specialPrompt =
+      "Does your password require special characters?";
+    special = confirm(specialPrompt);
+    console.log("special", special);
+  } // forces user to select at least one character type before leaving while loop
+  while (!lowercase && !uppercase && !numeric && !special)
+
+  // make strings for each character type
+  // combine into one string for all character types selected
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  console.log("lowercase", lowercaseChars.length);
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  console.log("uppercase", uppercaseChars.length);
+  const numbers = "0123456789"
+  console.log("numbers", numbers.length)
+  const specialChars = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  console.log("specialChars", specialChars);
   
-  const uppercasePrompt =
-    "Does your password require uppercase characters?";
-  const uppercase = confirm(uppercasePrompt);
-  console.log("uppercase", uppercase);
+  let availChars = '';
+  if (lowercase) {
+    availChars += lowercaseChars;
+  }
+  if (uppercase) {
+    availChars += uppercaseChars;
+  }
+  if (numeric) {
+    availChars += numbers;
+  }
+  if (special) {
+    availChars += specialChars;
+  }
 
-  const numericPrompt =
-    "Does your password require numeric characters?";
-  const numeric = confirm(numericPrompt);
-  console.log("numeric", numeric);
-
-  const specialPrompt =
-    "Does your password require special characters?";
-  const special = confirm(specialPrompt);
-  console.log("special", special);
-
-  // validate user input to have at least one character type
-
-  // unicode decimal for each character type
-  // lowercase = 97 - 122
-  // uppercase = 65 - 90
-  // numerical = 48 - 57
-  // special = 33 - 126 not including the above ranges
   let password = ''
+
+
   for (let i = 0; i < passwordLength; i++) {
-    let randChar = getRndInteger(97, 122);
-    password += String.fromCharCode(randChar);
   }
   return password
 }
 
 // generate random number between min & max (from w3schools)
 function getRndInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // Add event listener to generate button
